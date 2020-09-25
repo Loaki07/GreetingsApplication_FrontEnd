95f66f4881584d6818dd36b8c9fad334b67ade4d
sendo07 <loakesh.i@gmail.com>
2020-09-25 19:04:24 +0530

    add: Edit user form overlay

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 // UI Elements
-const addUserButtonUI = document.getElementById("add-user-btn"),
-  closeFormButtonUI = document.querySelector(".close-button");
+const addUserButton = document.getElementById("add-user-btn"),
+  editUserButton = document.getElementById("edit-user-btn"),
+  closeAddFormButton = document.querySelector(".close-button-for-add-user"),
+  closeEditFormButton = document.querySelector(".close-button-for-edit-user"),
+  addUserOverlay = document.querySelector(".add-user-modal"),
+  editUserOverlay = document.querySelector(".edit-user-modal");
 
 // Event Listeners
-addUserButtonUI.addEventListener("click", displayAddUserForm);
-closeFormButtonUI.addEventListener("click", closeForm);
+addUserButton.addEventListener("click", displayAddUserForm);
+editUserButton.addEventListener("click", displayEditUserForm);
+closeAddFormButton.addEventListener("click", closeAddUserForm);
+closeEditFormButton.addEventListener("click", closeEditUserForm);
 
 // Function to Display Add User Form
 function displayAddUserForm() {
-  document.querySelector(".add-user-modal").style.display = "flex";
+  addUserOverlay.style.display = "flex";
+}
+
+// Function to Display Edit User Form
+function displayEditUserForm() {
+  editUserOverlay.style.display = "flex";
 }
 
 // Function to close Add User Form
-function closeForm() {
-  document.querySelector(".add-user-modal").style.display = "none";
+function closeAddUserForm() {
+  addUserOverlay.style.display = "none";
+}
+
+function closeEditUserForm() {
+  editUserOverlay.style.display = "none";
 }
